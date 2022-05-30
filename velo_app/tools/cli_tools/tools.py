b0d6fcc4b3b28ac2f4e64e9819681dac2fd9d6ec
Jef <jef.v.d.a@live.be>
2022-05-30 08:31:01 +0200

    Updated clearing of console (cross-platform)

--- velo_app/tools/cli_tools/tools.py
+++ velo_app/tools/cli_tools/tools.py
@@ -2,7 +2,10 @@ import os
 
 
 def clear():
-    os.system('clear')
+    command = 'clear' # If Machine is running on Unix, use clear
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
 
 def wait_for_enter():
     input("Press enter to continue...")
