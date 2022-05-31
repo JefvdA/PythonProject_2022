25f0295dd39f638a02457e6eeb654296ba0973ac
Jef <jef.v.d.a@live.be>
2022-05-31 14:23:06 +0200

    Updated .gitignore
    Now also track constants file

---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+dirname = os.path.dirname(__file__)
+
+VELO_JSON = os.path.join(dirname, 'assets/velo.geojson')
+VELO_PICKLE = os.path.join(dirname, 'assets/velo.pickle')
+HTML_DIR = os.path.join(dirname, 'html')
