a7b4941078f85d5e1baa36c03150cf98031b03ea
jefvda <jef.v.d.a@live.be>
2022-06-02 15:42:18 +0200

    Added cli arg

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ Jef van der Avoirt - ITSOF1
 
 
 import os
+import sys
 
 from constants import VELO_PICKLE
 from velo_app import VeloApp
+from simulator import Simulator
 import screens.initialize_menu as init_menu
 import screens.main.main_menu as main_menu
 
@@ -23,8 +25,18 @@ def main():
     else: # If saved data is found, ask user what to do
         os.system('clear')
         init_menu.run(app)
-    
-    main_menu.run(app)
+
+    if len(sys.argv) > 1:
+        arg = sys.argv[1]
+        if arg == "-s":
+            if len(sys.argv) > 2:
+                time_multiplier = int(sys.argv[2])
+            else:
+                time_multiplier = 1
+            simulator = Simulator(app, time_multiplier)
+            simulator.start()
+    else:  
+        main_menu.run(app)
     
     app.save_data() # App is closing, save data
 
